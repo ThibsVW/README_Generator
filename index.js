60bfc6353f5d59c402e92e434471fcd94977d22e
thibs <thibault.vanweydeveld@hotmail.com>
2020-07-11 17:26:22 +0800

    inquirer fixed

--- index.js
+++ index.js
@@ -1,43 +1,88 @@
-
 var inquirer = require('inquirer');
 
-inquirer.prompt ([
+inquirer
+    .prompt([
+    {
+    type: 'input',
+    name: 'readme',
+    message: "Are you creating a README.md ? If yes, type README",
+    },
   {
     type: 'input',
-    name: 'repo name',
-    message: "What's your repository name?",
-    filter: function (val) {
-        return val.toLowerCase();
-      },
+    name: 'title',
+    message: "What's the name of you project",
   },
   {
     type: 'input',
-    name: 'introduction',
-    message: "An introduction or lead on what problem your application solves",
-    filter: function (val) {
-        return val.toLowerCase();
-      },
+    name: 'description',
+    message: "Explain what this project about?",
   },
   {
     type: 'input',
-    name: 'codes',
-    message: "Show a few code examples",
-    filter: function (val) {
-        return val.toLowerCase();
-      },
+    name: 'installation',
+    message: "What are the installatoin requirements?",
   },
   {
     type: 'input',
-    name: 'instruction',
-    message: "Explain how your application works",
-    filter: function (val) {
-        return val.toLowerCase();
-      },
+    name: 'usage',
+    message: "Details about using this code?",
   },
-
+  {
+    type: 'checkbox',
+    message: "What licence do you require?",
+    name: 'licences',
+    choices: [
+      new inquirer.Separator(' = licence = '),
+      {
+        name: 'Licence 1',
+      },
+      {
+        name: 'Licence 2',
+      },
+      {
+        name: 'Licence 3',
+      },
+      {
+        name: 'Other',
+      },
+    ]
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: "What is your Github username?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Your email address?",
+  },
 ])
 
-.then((answers) => {
-  console.log(JSON.stringify(answers, null, '  '));
-
+.then(function (response) {
+    //Require the FS module to create the MD file
+    const fs = require("fs");
+    
+    //Create the file and save the user's inputs
+    fs.writeFile(`${response.readme}.md`,
+        (`## Project Title: ${response.title} \n` +
+            `\n## Description: \n${response.description} \n` +
+            `\n## Table of Contents \n` +
+            `* [Installation](#Installation) \n` +
+            `* [Usage Information](#Usage) \n` +
+            `* [Licenses](#Licenses) \n` +
+            `* [Test Instructions](#Github) \n` +
+            `* [Questions](#Questions) \n` +
+            `* [License](#License)  \n` +
+            `\n## Installation: \n${response.installation} \n` +
+            `\n## Usage: \n${response.usage} \n` +
+            `\n## Licenses: \n${response.licenses} \n` +
+            `\n## Github Profile: \n${response.github} \n` +
+            `\n## Email me at ${response.email}. \n`), 
+        function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("ReadMe file generated.");
+        });
 });
